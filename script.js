f298dd735730256909394de29bbc9baeb139f115
Francis-maina <mainaf471@gmail.com>
2024-03-08 01:30:10 +0300

    javascript functionality

---
+++ script.js
@@ -0,0 +1,19 @@
+    var header = document.getElementById("header");
+    var ms = document.getElementById("models");
+    var m3 = document.getElementById("model3");
+    var mx = document.getElementById("modelx");
+    var my = document.getElementById("modely");
+
+
+    ms.onclick = function(){
+        header.style.backgroundImage = "url(Images/10101004_201510b.webp)";
+    }
+    m3.onclick = function(){
+        header.style.backgroundImage = "url(Images/1200px-Kokusai_Motorcars_Toyota_Crown_Majesta_S210.jpg)";
+    }
+    mx.onclick = function(){
+        header.style.backgroundImage = "url(Images/IMG-20230409-WA0002.jpg)";
+    }
+    my.onclick = function(){
+        header.style.backgroundImage = "url(Images/mlp-img-ext-2024-crown.jpg)";
+    }
